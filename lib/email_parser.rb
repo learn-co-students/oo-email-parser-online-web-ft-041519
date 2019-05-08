87d7e8be6eee9153c5321f4128c0f59af9766428
Arianna Kosten <arianna.kosten@gmail.com>
2019-05-08 21:01:50 +0000

    Done.

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -9,9 +9,8 @@ class EmailParser
   end
    
   def parse 
-    emails.split.collect do |e_address|     # why do i need split twice?
-      e_address.split(',').join("")   #takes the value, splits on the ,. use .join to elim. array[]
-    end.uniq          #runs .uniq on the return value last line of code of the line above.
+    emails.gsub("," , " ").split.uniq
   end
 end 
+
 
